9319d7aa155080d7229a613e302ae48ccad26cbb
Nicolas Casanova <nicasanovac@users.noreply.github.com>
2018-02-17 15:24:55 -0500

    Actualizacion casa

---
+++ IoGameJam2/Assets/Scripts/MovimientoJugadorCas.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoJugador : MonoBehaviour {
+
+    public float velocidad = 1.0f;
+
+    void Start()
+    {
+        Transform tras = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 movimiento = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += movimiento * velocidad * Time.deltaTime; 
+	}
+}
